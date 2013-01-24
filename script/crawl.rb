2c887e1549dd7fddfbcf01396266511db159dc6b
jungledrum <zhangbo13579@gmail.com>
2013-01-24 23:49:40 +0800

    完善爬虫，固定线程数，显示实时状态

--- script/crawl.rb
+++ script/crawl.rb
@@ -8,7 +8,7 @@ ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
   :host     => "localhost",
   :username => "root",
-  :password => "",
+  :password => "123456",
   :database => "pocket-search_development"
 )
 
@@ -18,37 +18,46 @@ end
 
 @bodys = Hash.new
 
-def crawl(link, links)
+def crawl(link)
   begin
     uri = URI(link.url)
     http = Net::HTTP.new(uri.host, uri.port)
     if uri.scheme == "https"
       http.use_ssl = true
     end
+    http.read_timeout = 3
+    http.open_timeout = 3
     res = http.get(uri.request_uri, {'User-Agent' => 'Chrome'})
     source = res.body
     @bodys[link.id] = {"content"=>Readability::Document.new(source).content, "crawl_status"=>res.code}
-    p links.size
+    # @bodys[link.id] = {"content"=>source, "crawl_status"=>res.code}
   rescue => detail
-    p "-"*10
-    p link.url
     p detail
-    @bodys[link.id] = {"crawl_status"=>404}
+    @bodys[link.id] = {"crawl_status"=>1000}
   end
+  # l = Link.find(link.id)
+  # l.update_attributes("content"=>@bodys[link.id]["content"], "crawl_status"=>@bodys[link.id]["crawl_status"])
 end
 
-links = Link.where("crawl_status is NULL")
+links = Link.where("crawl_status = 200").order("id DESC")
+links = links[0,links.size]
 p links.size
-links = links[0,30]
 threads = []
 
-5.times do
+links_size = links.size
+thread_num = 20
+start_time = Time.now
+thread_num.times do |index|
   begin
     threads << Thread.new do
       until links.blank?
         link = links.pop
-        crawl(link, links)
+        t1 = Time.now
+        crawl(link)
+        t2 = Time.now
+        puts "T#{index}\t#{links.size}\t#{t2-t1}\t#{link.title[0,20]}\n"
       end
+      p "Thread #{index} exit."
       Thread.current.exit 
     end
   rescue => detail
@@ -57,11 +66,15 @@ threads = []
   end
 end
 
-p threads
 threads.each do |x|
   x.join
 end
 
-links.each_with_index do |link, index|
-  link.update_attributes("content"=>@bodys[link.id]["content"], "crawl_status"=>@bodys[link.id]["crawl_status"])
-end
+end_time = Time.now
+p "consume_time:#{end_time - start_time}, worker:#{thread_num}, links_size:#{links_size}"
+
+p "=======END========="
+# @bodys.each do |k, v|
+#   link = Link.find(k)
+#   link.update_attributes("content"=>v["content"], "crawl_status"=>v["crawl_status"])
+# end
